a44500699ae72d8fcb5bc370eed6e13e1ef1e02a
Sanskruti-Kokare-19 <sanskruti.202014207@vcet.edu.in>
2024-08-18 15:59:30 +0530

    sec commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6ivbYlTYpjnc0Ak8Wl2EwgmKEMA88Ihc",
+  authDomain: "chatapp7-adc9e.firebaseapp.com",
+  projectId: "chatapp7-adc9e",
+  storageBucket: "chatapp7-adc9e.appspot.com",
+  messagingSenderId: "42179671212",
+  appId: "1:42179671212:web:c8e5a7b76f62b2fb209b09"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
